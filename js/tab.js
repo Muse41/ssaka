3882f7e7e346f8f1918b244a87efaf5226786434
muse <jisik24@naver.com>
2022-09-14 09:44:27 +0900

    Update tab.js

--- js/tab.js
+++ js/tab.js
@@ -10,7 +10,8 @@ $(function(){
         anchor.each(function(){
             var href = anchor.attr('href');
 
-            $(this).click(function(){
+            $(this).click(function(event){
+                event.preventDefault(); //
                 href = $(this).attr('href');
                 anchor.removeClass('on');
                 $(this).addClass('on');
@@ -20,23 +21,30 @@ $(function(){
         })
     })
 
-    $('#nav_btn').click(function(){
-        $('.nav_pop').animate({left:0},400);
+    $('#bars').click(function(){
+        $('#nav_pop').animate({left:'0'},400);
     })
 
-    $('.close_btn').click(function(){
-        $('.nav_pop').animate({left:'-100%'},400); //%나 px같이 단위 들어갈땐 반드시 ' ' 따옴표를 붙여야 함!!
+    $('.panels .close').click(function(){
+        $('#nav_pop').animate({left:'-100%'},400); //%나 px같이 단위 들어갈땐 반드시 ' ' 따옴표를 붙여야 함!!
     })
 
     $('#btn1 > li.link_on .sub').show();
     $('#btn1 > li').each(function(){
         $(this).click(function(){     
             var sub = $(this).find('.sub')
-            /*  $('#btn1 > li').removeClass('link_on');
+         $('#btn1 > li').removeClass('link_on');
          $(this).addClass('link_on'); 
-           $(sub).slideUp();         
-            $(sub).slideDown();*/           
-            $(sub).slideToggle();
-        })
+                
+            //$(sub).slideDown(); 
+
+           $('.sub').slideUp();
+             $(sub).slideToggle();             
+
+        
     })
-})///
+})  
+
+        
+
+})
